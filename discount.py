4d07df4f31454a7e365fdbf6cd329e9d7e8a59e5
The coding cove <lawrencewafula408@gmail.com>
2025-04-17 11:03:45 +0300

    Create discount.py

---
+++ discount.py
@@ -0,0 +1,18 @@
+'''
+Create a function named calculate_discount(price, discount_percent) that calculates the final price after applying a discount. 
+The function should take the original price (price) and the discount percentage (discount_percent) as parameters. 
+If the discount is 20% or higher, apply the discount; otherwise, return the original price.
+Using the calculate_discount function, prompt the user to enter the original price of an item and the discount percentage. 
+Print the final price after applying the discount, or if no discount was applied, print the original price.
+'''
+
+def calculate_discount(price, discount_percent):
+  if discount_percent >= 20:
+    price = price
+  else:
+    price = price * discount_percent / 100
+
+price = int(input("Please enter the original price of the item"))
+discount_percent = int(input("Enter the discount percent"))
+
+print(calulate_discount(price, discount_percent))
